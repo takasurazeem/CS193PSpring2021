13ac235c599ddc537c4aa0c23e38837921147613
Takasur Azeem <takasur@nullbrainer.io>
2021-06-05 12:10:15 +0500

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Takasur Azeem on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
